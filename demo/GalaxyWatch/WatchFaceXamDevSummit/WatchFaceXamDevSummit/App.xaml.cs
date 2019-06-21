a74eab60a5078d33678bef183f94bfc3af4fdcdc
Kangho Hur <kangho.hur@samsung.com>
2019-06-21 14:42:02 +0900

    Add WatchFace for XamDevSummit

---
+++ demo/GalaxyWatch/WatchFaceXamDevSummit/WatchFaceXamDevSummit/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WatchFaceXamDevSummit
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+    }
+}
